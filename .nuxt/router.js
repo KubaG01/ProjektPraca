7df1b99ba114da45b53e93b1b79ddf9e6b81a551
KubaG01 <s210857@sggw.edu.pl>
2024-05-23 09:57:12 +0200

    Dodanie .nuxt

---
+++ .nuxt/router.js
@@ -0,0 +1,79 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import { normalizeURL, decode } from 'ufo'
+import { interopDefault } from './utils'
+import scrollBehavior from './router.scrollBehavior.js'
+
+const _4f6071a3 = () => interopDefault(import('..\\pages\\index.vue' /* webpackChunkName: "pages/index" */))
+const _5dc50366 = () => interopDefault(import('..\\pages\\aplications.vue' /* webpackChunkName: "pages/aplications" */))
+const _8880edbe = () => interopDefault(import('..\\pages\\servers.vue' /* webpackChunkName: "pages/servers" */))
+const _e953f342 = () => interopDefault(import('..\\pages\\tasks.vue' /* webpackChunkName: "pages/tasks" */))
+
+const emptyFn = () => {}
+
+Vue.use(Router)
+
+export const routerOptions = {
+  mode: 'history',
+  base: '/',
+  linkActiveClass: 'nuxt-link-active',
+  linkExactActiveClass: 'nuxt-link-exact-active',
+  scrollBehavior,
+
+  routes: [{
+    path: "/en",
+    component: _4f6071a3,
+    name: "index___en"
+  }, {
+    path: "/pl",
+    component: _4f6071a3,
+    name: "index___pl"
+  }, {
+    path: "/en/aplications",
+    component: _5dc50366,
+    name: "aplications___en"
+  }, {
+    path: "/en/servers",
+    component: _8880edbe,
+    name: "servers___en"
+  }, {
+    path: "/en/tasks",
+    component: _e953f342,
+    name: "tasks___en"
+  }, {
+    path: "/pl/aplications",
+    component: _5dc50366,
+    name: "aplications___pl"
+  }, {
+    path: "/pl/servers",
+    component: _8880edbe,
+    name: "servers___pl"
+  }, {
+    path: "/pl/tasks",
+    component: _e953f342,
+    name: "tasks___pl"
+  }],
+
+  fallback: false
+}
+
+export function createRouter (ssrContext, config) {
+  const base = (config._app && config._app.basePath) || routerOptions.base
+  const router = new Router({ ...routerOptions, base  })
+
+  // TODO: remove in Nuxt 3
+  const originalPush = router.push
+  router.push = function push (location, onComplete = emptyFn, onAbort) {
+    return originalPush.call(this, location, onComplete, onAbort)
+  }
+
+  const resolve = router.resolve.bind(router)
+  router.resolve = (to, current, append) => {
+    if (typeof to === 'string') {
+      to = normalizeURL(to)
+    }
+    return resolve(to, current, append)
+  }
+
+  return router
+}
